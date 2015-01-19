5739aed069c27c105ca720204ac32621c3cd8a00
Jacinda Zhong <jacindaz@gmail.com>
2015-01-19 07:19:55 -0500

    All tests passing for user and post, adds custom validation message

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,6 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   validates :email, presence: true
-  validates_format_of :email, with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create
+  validates_format_of :email, with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create, message: 'must be a valid email address'
 
 end
