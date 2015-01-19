4bc70bc602700d89beabf9d547dc5fc709145c26
Jacinda Zhong <jacindaz@gmail.com>
2015-01-18 19:03:48 -0500

    Adds regex for user email validation

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,7 +8,11 @@ describe User do
 
   it "is invalid when email is blank" do
     user = FactoryGirl.build(:user, email: '')
+    expect(user).not_to be_valid
+  end
 
+  it "is invalid when doesn't include @ sign" do
+    user = FactoryGirl.build(:user, email: 'lalala')
     expect(user).not_to be_valid
   end
 
