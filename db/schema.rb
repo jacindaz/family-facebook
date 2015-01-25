25dd1f9d278eedd0c8e7e7e88d7038678c767926
Jacinda Zhong <jacindaz@gmail.com>
2015-01-25 06:33:25 -0500

    Sets up devise "confirmable" module
     - adds fields to Users table for "confirmable"
     - adds action_mailer configuration to app.rb
     - sets up :file for confirmation emails

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150124234012) do
+ActiveRecord::Schema.define(version: 20150125105935) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 20150124234012) do
     t.string   "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
+    t.boolean  "unconfirmed_email"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
