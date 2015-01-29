64203e288f1cdc7fe721df61eda7ae774ae84ff2
Jacinda Zhong <jacindaz@gmail.com>
2015-01-29 06:44:35 -0500

    Adds paperclip method and validation of photo

--- app/models/post.rb
+++ app/models/post.rb
@@ -10,4 +10,7 @@ class Post < ActiveRecord::Base
   validates :user_id, presence: true, numericality: { only_integer: true }
 
   belongs_to :user
+
+  has_attached_file :photo, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+  validates_attachment_content_type :photo, :content_type => /\Aimage\/.*\Z/
 end
