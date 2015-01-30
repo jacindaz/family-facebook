b59c5cd8978ba77364a88146c5f2e4a3ba63e1a9
Jacinda Zhong <jacindaz@gmail.com>
2015-01-30 08:22:33 -0500

    Validates photo content_type and size

--- app/models/post.rb
+++ app/models/post.rb
@@ -12,5 +12,5 @@ class Post < ActiveRecord::Base
   belongs_to :user
 
   has_attached_file :photo, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
-  validates_attachment_content_type :photo, :content_type => /\Aimage\/.*\Z/
+  validates_attachment :photo, content_type: { content_type: /\Aimage\/.*\Z/ }, size: { in: 0..5.megabytes }
 end
