931c459a15cf0006e933b8c1ede910b18741a207
Jacinda Zhong <jacindaz@gmail.com>
2015-01-30 08:23:10 -0500

    Migration to add avatar to Users table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150129104405) do
+ActiveRecord::Schema.define(version: 20150129123458) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,6 +46,10 @@ ActiveRecord::Schema.define(version: 20150129104405) do
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.boolean  "unconfirmed_email"
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
